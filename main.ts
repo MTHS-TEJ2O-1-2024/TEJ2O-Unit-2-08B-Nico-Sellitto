3324b0242124e77406810f423d8313539d844aa5
Nico-Sellitto <nico.sellitto@mths.ca>
2024-10-03 10:19:04 -0400

    changed text

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Nico Sellitto
+ * Created on: oct 2024
+ * This program scrolls through LED colors
 */
 
-basic.showString('Hello, World!')
+
