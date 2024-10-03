6940d5f6c786e10746ca5ab62665f92e01114a4d
Nico-Sellitto <nico.sellitto@mths.ca>
2024-10-03 10:51:11 -0400

    edited code

--- main.ts
+++ main.ts
@@ -14,28 +14,61 @@ pins.digitalWritePin(DigitalPin.P16, 0)
 // scrolls through colours when a is pressed
 input.onButtonPressed(Button.A, function() {
     
+    // red
     pins.digitalWritePin(DigitalPin.P14, 1)
     basic.pause(1000)
+
     pins.digitalWritePin(DigitalPin.P14, 0)
-    pins.digitalWritePin(DigitalPin.P15, 1)
-    basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P15, 0)
-    pins.digitalWritePin(DigitalPin.P16, 1)
-    basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P16, 0)
+
+    // blue
+    pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P15, 1)
-    pins.digitalWritePin(DigitalPin.P14, 1)
     basic.pause(1000)
+
+    pins.digitalWritePin(DigitalPin.P14, 0)
     pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+
+    // green
+    pins.digitalWritePin(DigitalPin.P16, 1)
+    basic.pause(1000)
+
     pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+
+    // purple
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    basic.pause(1000)
+
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+
+    // yellow
     pins.digitalWritePin(DigitalPin.P16, 1)
     pins.digitalWritePin(DigitalPin.P14, 1)
     basic.pause(1000)
-    pins.digitalWritePin(DigitalPin.P16, 0)
+
     pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+
+    // cyan
     pins.digitalWritePin(DigitalPin.P15, 1)
     pins.digitalWritePin(DigitalPin.P16, 1)
     basic.pause(1000)
+
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
+
+    // white
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    pins.digitalWritePin(DigitalPin.P16, 1)
     pins.digitalWritePin(DigitalPin.P14, 1)
     basic.pause(1000)
     pins.digitalWritePin(DigitalPin.P15, 0)
